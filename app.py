4dd9e8eeb13b0a620618ef512d8be70d50547aa8
DURGA VENU KUMAR MUTYALA <145457524+thevk22@users.noreply.github.com>
2023-12-11 12:48:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,69 +9,100 @@ from langchain.chains import ConversationalRetrievalChain
 from langchain.memory import ConversationBufferMemory
 import os
 
-os.environ['GOOGLE_API_KEY'] =  st.secrets["API_KEY"]
+os.environ['GOOGLE_API_KEY'] = st.secrets.get("API_KEY", "")
 
 def get_pdf_text(pdf_docs):
-    text=""
-    for pdf in pdf_docs:
-        pdf_reader= PdfReader(pdf)
-        for page in pdf_reader.pages:
-            text+= page.extract_text()
-    return  text
+    text = ""
+    try:
+        for pdf in pdf_docs:
+            pdf_reader = PdfReader(pdf)
+            for page in pdf_reader.pages:
+                text += page.extract_text()
+        return text
+    except Exception as e:
+        st.error(f"Error extracting text from PDF: {str(e)}")
 
 def get_text_chunks(text):
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=20)
-    chunks = text_splitter.split_text(text)
-    return chunks
+    try:
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=20)
+        chunks = text_splitter.split_text(text)
+        return chunks
+    except Exception as e:
+        st.error(f"Error splitting text into chunks: {str(e)}")
 
 def get_vector_store(text_chunks):
-    embeddings = GooglePalmEmbeddings()
-    vector_store = FAISS.from_texts(text_chunks, embedding=embeddings)
-    return vector_store
+    try:
+        embeddings = GooglePalmEmbeddings()
+        vector_store = FAISS.from_texts(text_chunks, embedding=embeddings)
+        return vector_store
+    except Exception as e:
+        st.error(f"Error creating vector store: {str(e)}")
 
 def get_conversational_chain(vector_store):
-    llm=GooglePalm()
-    memory = ConversationBufferMemory(memory_key = "chat_history", return_messages=True)
-    conversation_chain = ConversationalRetrievalChain.from_llm(llm=llm, retriever=vector_store.as_retriever(), memory=memory)
-    return conversation_chain
+    try:
+        llm = GooglePalm()
+        memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+        conversation_chain = ConversationalRetrievalChain.from_llm(llm=llm, retriever=vector_store.as_retriever(), memory=memory)
+        return conversation_chain
+    except Exception as e:
+        st.error(f"Error creating conversational chain: {str(e)}")
 
 def user_input(user_question):
-    response = st.session_state.conversation({'question': user_question})
-    st.session_state.chatHistory = response['chat_history']
-    for i, message in enumerate(st.session_state.chatHistory):
-        if i%2 == 0:
-            st.write("Human: ", message.content)
-        else:
-            st.write("Bot: ", message.content)
+    try:
+        response = st.session_state.conversation({'question': user_question})
+        st.session_state.chatHistory = response['chat_history']
+        for i, message in enumerate(st.session_state.chatHistory):
+            if i % 2 == 0:
+                st.write("Human: ", message.content)
+            else:
+                st.write("Bot: ", message.content)
+    except Exception as e:
+        st.error(f"Error processing user input: {str(e)}")
+
 def main():
     st.set_page_config("Veddy AI")
     st.header("Chat with your PDF 💬")
+    
+    # Check if PDFs are uploaded
+    if "pdf_docs" not in st.session_state:
+        st.warning("Open the left sidebar to upload your PDF and click 'Process'")
+        return
+    
     user_question = st.text_input("Ask a Question from the PDF Files")
+    
+    # Check if PDFs are uploaded before processing user input
+    if not st.session_state.pdf_docs and user_question:
+        st.warning("Upload your PDF files first before asking a question.")
+        return
+    
     if "conversation" not in st.session_state:
         st.session_state.conversation = None
     if "chatHistory" not in st.session_state:
         st.session_state.chatHistory = None
+    
     if user_question:
         user_input(user_question)
+    
     with st.sidebar:
         st.title("Settings")
         st.subheader("Upload your Documents")
-        pdf_docs = st.file_uploader("Upload your PDF Files and Click on the Process Button", accept_multiple_files=True)
+        st.warning("Open the sidebar to upload your PDF and click 'Process'")
+        pdf_docs = st.file_uploader("Upload your PDF Files and Click on the Process Button", accept_multiple_files=True, key="pdf_docs")
+        
         if st.button("Process"):
-            with st.spinner("Processing"):
-                raw_text = get_pdf_text(pdf_docs)
-                text_chunks = get_text_chunks(raw_text)
-                vector_store = get_vector_store(text_chunks)
-                st.session_state.conversation = get_conversational_chain(vector_store)
-                st.success("Done")
-                st.markdown(
-            """
-            <div style="position: fixed; bottom: 10px; left: 10px; background-color: #ff4b4b; padding: 10px; border-radius: 8px; color: white;">
-                Thevk22
-            </div>
-            """,
-            unsafe_allow_html=True
-        )
+            if not pdf_docs:
+                st.warning("Upload your PDF files first.")
+            else:
+                with st.spinner("Processing"):
+                    try:
+                        raw_text = get_pdf_text(pdf_docs)
+                        text_chunks = get_text_chunks(raw_text)
+                        vector_store = get_vector_store(text_chunks)
+                        st.session_state.conversation = get_conversational_chain(vector_store)
+                        st.success("Done")
+                    except Exception as e:
+                        st.error(f"Error processing PDF documents: {str(e)}")
+
     hide_streamlit_style = """
             <style>
             [data-testid="stToolbar"] {visibility: hidden !important;}
